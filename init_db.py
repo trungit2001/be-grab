9c6d67ab0a2f2d71911d4fa1141d552ca04fba59
Phạm Thành Trung <thanhtrungit2001@gmail.com>
2022-11-05 04:12:51 +0700

    change way to check exist data

--- init_db.py
+++ init_db.py
@@ -6,16 +6,32 @@ def create_database():
 
 
 def main():
-    if metadata:
-        print("[WARNING] Check the database exists. You may want to delete? (y/N):", end=' ')
+    stmt = '''
+        SELECT 
+            schemaname as schema_name,
+            relname as table_name, 
+            n_live_tup as record_count 
+        FROM pg_stat_user_tables WHERE n_live_tup > 0;
+    '''
+    conn = engine.connect()
+    result = conn.execute(stmt).fetchall()
+    conn.close()
+
+    if result:
+        print("[WARNING] Check the database exists data. You may want to delete? (Y/n):", end=' ')
         choose = input()
         if choose in ["y", "Y"]:
             metadata.drop_all()
+            print("[INFO] Database deleted!")
+
             metadata.create_all()
             create_database()
-            print("[INFO] Database created!")
+            print("[INFO] Database initialized!")
         else:
             print("[INFO] Database exists, cannot be initialized!")
+    else:
+        create_database()
+        print("[INFO] Database initialized!")
 
 
 if __name__ == "__main__":
